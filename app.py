8706114807f6a1e034acf3ca278ab83e02627d47
vahooz <mail@robertwachowiak.com>
2025-06-18 10:18:57 +0200

    test5
    test5

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ def hello():
     query = data.get('query', '')
     return jsonify({"message": query})
 
+@app.route('/')
+def welcome():
+    return "<h1>Welcome to My Flask App!</h1><p>This is a simple welcome page.</p>"
+
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=5000, debug=True)
